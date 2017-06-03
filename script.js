bfee9b766b4426c8a84ff0dcde23c23e83d399f5
sa666u <alex_ivkov@yahoo.com>
2017-06-03 19:35:49 +0100

    New navbar and layout fixes

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 if ($('#back-to-top').length) {
-    var scrollTrigger = 500, // px
-        backToTop = function () {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('#back-to-top').addClass('show');
-            } else {
-                $('#back-to-top').removeClass('show');
-            }
-        };
-    backToTop();
-    $(window).on('scroll', function () {
-        backToTop();
-    });
-    $('#back-to-top').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
-    });
+	var scrollTrigger = 500, // px
+		backToTop = function () {
+			var scrollTop = $(window).scrollTop();
+			if (scrollTop > scrollTrigger) {
+				$('#back-to-top').addClass('show');
+			} else {
+				$('#back-to-top').removeClass('show');
+			}
+		};
+	backToTop();
+	$(window).on('scroll', function () {
+		backToTop();
+	});
+	$('#back-to-top').on('click', function (e) {
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		}, 700);
+	});
 }
 
 
 // Select all links with hashes
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+			&&
+			location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					// Callback after animation
+					// Must change focus!
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) { // Checking if the target was focused
+						return false;
+					} else {
+						$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					};
+				});
+			}
+		}
+	});
 
 $('.nav.navbar-nav > li').on('click', function(e) {
-    $('.nav.navbar-nav > li').removeClass('active');
-    $(this).addClass('active');
-});    
+	$('.nav.navbar-nav > li').removeClass('active');
+	$(this).addClass('active');
+});
 
 
 $("#navContacts").click(function(){
-    $("#myContacts").css("display", "none").show(1500);
+	$("#myContacts").css("display", "none").show(1500);
 });
 
 
-$("#navPortfolio").click(function(){   
- 
-  $("#myExperience").delay("fast").css("display", "none").show(1200); 
+$("#navPortfolio").click(function(){
+
+	$("#myExperience").delay("fast").css("display", "none").show(1200);
 });
 
-$("#navAbout").click(function(){   
-    $("#aboutMe").delay("fast").css("display", "none").show(1200); 
+$("#navAbout").click(function(){
+	$("#aboutMe").delay("fast").css("display", "none").show(1200);
 });
 
 $( document ).ready(function() {
-    $( "#aboutMe" ).css("display", "none").show(1200);
-  $( "#myExperience" ).show(); 
-  $( "#myContacts" ).show(); 
+	$( "#aboutMe" ).css("display", "none").show(1200);
+	$( "#myExperience" ).show();
+	$( "#myContacts" ).show();
 });
 
-$('#back-to-top').click(function(){   
- $('.nav.navbar-nav > li').removeClass('active');
- $("#navHome").addClass('active');
+$('#back-to-top').click(function(){
+	$('.nav.navbar-nav > li').removeClass('active');
+	$("#navHome").addClass('active');
 });
 
 
-                       
-                      
+
+
